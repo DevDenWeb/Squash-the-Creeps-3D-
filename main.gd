1cfa4c0d73efab36f96ccaa1a5052f62e9a9bfc6
Denis <nosovdenis14@gmail.com>
2025-01-11 02:00:54 +0300

    Create new script "main" and added node "Path3D"

---
+++ main.gd
@@ -0,0 +1,20 @@
+extends Node
+
+@export var mob_scene: PackedScene
+
+
+
+func _on_mob_timer_timeout():
+	# Создаёт новый экземпляр сцены "mob"
+	var mob = mob_scene.instantiate()
+	
+	# Выбирает случайное местополоение на SpawnPath. Сохраняет ссылку на узел SpawnLocation
+	var mob_spawn_location = get_node("SpawnPath/SpawnLocation")
+	# Задаёт ему случайное смещение от 0 до 1
+	mob_spawn_location.progress_ratio = randf()
+	
+	var player_position = $Player.position
+	mob.initialize(mob_spawn_location.position, player_position)
+	
+	# Создаёт моба, добавив его на главную сцену
+	add_child(mob)
